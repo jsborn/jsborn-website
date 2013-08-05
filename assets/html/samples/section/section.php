c56590c8ae061cfad4d2ddf46606ece8affbd944
tureki <tureki11@gmail.com>
2013-08-05 23:27:35 +0800

    site update
    site update

--- assets/html/samples/section/section.php
+++ assets/html/samples/section/section.php
@@ -2,5 +2,5 @@
 	<iframe 
 		src="<?php echo DIR_ROOT; ?>/assets/html/samples/section/sample-01/index.html" 
 		width="100%"
-		height="1000" allowtransparency="true" frameborder="0" scrolling="0"></iframe>
+		height="1000" allowtransparency="true" frameborder="0" scrolling="0" style="border: 1px solid #E5E5E5;"></iframe>
 </div>
